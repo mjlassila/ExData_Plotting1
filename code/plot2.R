b11a19c43b1ec59d66f002cc9f575446cf8ff2fb
Matti Lassila <matti.lassila@gmail.com>
2014-05-11 21:20:21 +0300

    Code and graphics for plot 2

---
+++ code/plot2.R
@@ -0,0 +1,23 @@
+source.and.set <- function(x) {
+  setwd(dirname(x))
+  source(x)
+}
+
+source.and.set("load_and_prepare_data.R")
+
+
+# Plot 2
+
+png(file="../output/plot2.png", width=480,height=480)
+
+plot(
+  df$Timestamp,
+  df$Global_active_power,
+  ylab = "Global Active Power (kilowatts)",
+  xlab = '',
+  type = "l",
+  lwd = 1,
+  col = "black"
+)
+
+dev.off()

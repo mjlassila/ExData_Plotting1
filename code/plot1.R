9dbcab25b14bd94af9c52e7ea9ea806fee6efa2a
Matti Lassila <matti.lassila@gmail.com>
2014-05-11 21:20:07 +0300

    Code and graphics for plot 1

---
+++ code/plot1.R
@@ -0,0 +1,21 @@
+source.and.set <- function(x) {
+  setwd(dirname(x))
+  source(x)
+}
+
+source.and.set("load_and_prepare_data.R")
+
+
+
+# Plot 1
+
+png(file="../output/plot1.png", width=480,height=480)
+
+hist(
+  df$Global_active_power,
+  xlab = "Global Active Power (kilowatts)",
+  main ="Global Active Power",
+  col = "red"
+  )
+
+dev.off()
